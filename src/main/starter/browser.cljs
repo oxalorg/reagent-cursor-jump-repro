8405dd4f6b2d41c5a56e3cbeb90240c6da0a9490
Mitesh Shah <mitesh@miteshshah.com>
2025-07-23 12:59:20 +0530

    cursor jump reproduction

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,8 +1,74 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            ["react-dom/client" :as rdom-client]
+            [shadow.cljs.modern :refer [defclass]]))
+
+(defonce shadow-value (r/atom ""))
+(defonce normal-value (r/atom ""))
+(defonce rendered? (r/atom false))
+
+(defn shadow-input-component []
+  [:div {:style {:padding "20px" :background "lightgray"}}
+   [:h3 "Shadow DOM Component"]
+   [:p "Write something in the box below, then move the cursor (to anything but last position) and write something again. It will move the cursor."]
+   [:input {:type "text"
+            :placeholder "Shadow DOM input"
+            :value @shadow-value
+            :on-change #(reset! shadow-value (.. % -target -value))
+            :style {:padding "8px"
+                    :border "2px solid #ccc"
+                    :border-radius "4px"
+                    :font-size "16px"
+                    :width "200px"}}]
+   [:p "Shadow input value: " @shadow-value]])
+
+(defclass ShadowInput (extends js/HTMLElement)
+  (constructor
+   [this]
+   (super)
+   (js/console.log "Rendering shadow input")
+   (let [shadow (.attachShadow this #js {:mode "open"})
+         container (.createElement js/document "div")
+         root (.createRoot rdom-client container)]
+     (.appendChild shadow container)
+     (.render root (r/as-element [shadow-input-component])))))
+
+(defn normal-input []
+  [:div
+   [:h3 "Normal Component"]
+   [:input {:type "text"
+            :placeholder "Normal input"
+            :value @normal-value
+            :on-change #(reset! normal-value (.. % -target -value))
+            :style {:padding "8px"
+                    :border "2px solid #ccc"
+                    :border-radius "4px"
+                    :font-size "16px"
+                    :width "200px"}}]
+   [:p "Normal input value: " @normal-value]])
+
+(defn normal-app []
+  [:div {:style {:padding "20px"}}
+   [normal-input]])
+
+(defn setup-shadow-component []
+  (when-not (.get js/customElements "shadow-input")
+    (.define js/customElements "shadow-input" ShadowInput))
+  (let [shadow-app-div (.getElementById js/document "shadow-app")
+        shadow-element (.createElement js/document "shadow-input")]
+    (set! (.-innerHTML shadow-app-div) "")
+    (.appendChild shadow-app-div shadow-element)))
+
+(defonce app-root (atom nil))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
-  (js/console.log "start"))
+  (js/console.log "start")
+  (when-not @app-root
+    (reset! app-root (.createRoot rdom-client (.getElementById js/document "app"))))
+  (.render @app-root (r/as-element [normal-app]))
+  (setup-shadow-component))
 
 (defn init []
   ;; init is called ONCE when the page loads
